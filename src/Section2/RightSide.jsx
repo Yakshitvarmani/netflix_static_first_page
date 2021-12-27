3291817d219af247039e45d36df04e86f596305c
yakshit <yakshitvermani777@gmail.com>
2021-12-27 17:31:36 +0530

    netflix static welcome page

---
+++ src/Section2/RightSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSide = () => {
+    return (
+        <div id="sec2rightblock">
+            <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" alt="tv"/>
+        </div>
+    )
+}
+
+export default RightSide
---
+++ src/Section5/Section5.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Section5.css"
+
+const Section5 = () => {
+    return (
+        <div id="sec3">
+            <div id="sec3left">
+                <img src="https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png?r=5cf" alt="mobile"/>
+
+            </div>
+            <div id="sec3right">
+                <h1>Create profiles for children.</h1>
+                <h3>Send children on adventures with their favourite characters in a space made just for them—free with your membership.</h3>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Section5
